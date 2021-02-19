e799a3bc4be9facf54c2108ff0f9b5c96350c7aa
Dominick Galluzzo <dom.galluzzo@gmail.com>
2021-02-19 12:50:26 -0500

    new cocktail _form

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
-    resources :doses, only: [:new, :create]
+  resources :cocktails, only: %i[index show new create destroy] do
+    resources :doses, only: %i[new create]
   end
   resources :doses, only: [:destroy]
 
-  root to: "cocktails#index"
+  root to: 'cocktails#index'
 end
